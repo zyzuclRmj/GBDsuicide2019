77c568162c37114b7ffb6d590e3cc64ea1563dea
Ziyi Zhao <rmjlzz1@ucl.ac.uk>
2024-05-22 14:00:56 +0100

    Import the datasets downloaded from GBD websites.

--- code for manuscript.R
+++ code for manuscript.R
@@ -21,3 +21,36 @@ Sys.setenv(R.GSCMD = "C:/Program Files/gs/gs9.52/bin/gswin64c.exe")
 library(ggrepel)
 library(ggpubr)
 library(patchwork)
+
+# import datasets
+country1 <- read_csv("C:/Users/zzhao/Desktop/global suicide/data2019/countries and terrorities/IHME-GBD_2019_DATA-17f54449-1.csv") %>% 
+  janitor::clean_names()
+
+country2 <- read_csv("C:/Users/zzhao/Desktop/global suicide/data2019/countries and terrorities/IHME-GBD_2019_DATA-17f54449-2.csv") %>% 
+  janitor::clean_names()
+
+country3 <- read_csv("C:/Users/zzhao/Desktop/global suicide/data2019/countries and terrorities/IHME-GBD_2019_DATA-17f54449-3.csv") %>% 
+  janitor::clean_names()
+
+global <- read_csv("C:/Users/zzhao/Desktop/global suicide/data2019/countries and terrorities/global.csv") %>% 
+  janitor::clean_names()
+
+country <- rbind(country1,country2,country3,global)
+
+regions <- read_csv("C:/Users/zzhao/Desktop/global suicide/data2019/regions.csv") %>% 
+  janitor::clean_names()
+
+super_regions <- 
+  read_csv("C:/Users/zzhao/Desktop/global suicide/data2019/super_regions.csv") %>% 
+  janitor::clean_names()
+
+ctry_abbr <- read_csv("C:/Users/zzhao/Desktop/global suicide/data/countries abbr.csv") %>% 
+  janitor::clean_names()
+
+sdi <- read_csv("C:/Users/zzhao/Desktop/global suicide/data2019/sdi.csv")
+
+# Table 1. Number of OA suicides, ASSR per 100,000, percent-change from 1990 to 2019 with UI, and OA-NOA suicide ratios in 2019 for all regions.
+
+
+
+
